b45b16a109e7a3a89d799096a7749b9001a8bddd
vinu cyril <38722628+vinuman@users.noreply.github.com>
2023-02-10 18:30:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your code here
+const form = document.getElementById("form");
+const result = document.getElementById("result");
+
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const input = document.querySelector("input[type='text']").value;
+  const arr = input.split(",").map(val => parseInt(val));
+  const minCost = getMinCost(arr);
+  result.textContent = minCost;
+});
+
+function getMinCost(arr) {
+  arr.sort((a, b) => a - b);
+  let cost = 0;
+  while (arr.length > 1) {
+    const first = arr.shift();
+    const second = arr.shift();
+    cost += first + second;
+    arr.unshift(first + second);
+    arr.sort((a, b) => a - b);
+  }
+  return cost;
+}
